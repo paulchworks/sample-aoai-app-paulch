54fcd111e218dd222913be6b527812e6a47a7462
PaulchWorks <107820572+paulchworks@users.noreply.github.com>
2025-01-25 22:21:27 +0800

    Update Button.tsx

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -1,17 +1,18 @@
-import { CommandBarButton, DefaultButton, IButtonProps } from '@fluentui/react'
+import { CommandBarButton, DefaultButton, IButtonProps } from "@fluentui/react";
 
-import styles from './Button.module.css'
+import styles from './Button.module.css';
 
 interface ButtonProps extends IButtonProps {
-  onClick: () => void
-  text: string | undefined
+  onClick: () => void;
+  text: string | undefined;
 }
 
 export const ShareButton: React.FC<ButtonProps> = ({ onClick, text }) => {
+
   return (
     <CommandBarButton
       className={styles.shareButtonRoot}
-      iconProps={{ iconName: 'Share' }}
+      iconProps={{ iconName: 'Logout' }}
       onClick={onClick}
       text={text}
     />
